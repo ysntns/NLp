e6f507192f6682444f9f81f7cbf6cc56fc9e81c7
Yasin Tanış <164824707+ysntns@users.noreply.github.com>
2024-05-29 14:48:59 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import transformers
+from transformers import pipeline
+import PyPDF2
 import streamlit as st
+from googletrans import Translator
 from transformers import BartTokenizer, BartForConditionalGeneration
 
 # Türkçe metin özetleme modeli ve tokenizer yükleniyor
@@ -9,6 +12,30 @@ tokenizer = BartTokenizer.from_pretrained("facebook/bart-large-cnn")
 # Streamlit arayüzü oluşturuluyor
 st.title("Türkçe Metin Özetleme Uygulaması")
 text_input = st.text_area("Metni buraya girin")
+
+def summarize_pdf(file, target_language):
+    # PDF'den metin oku
+    text = read_pdf(file)
+
+    # Metni özetle
+    summary = summarizer(text, max_length=150, min_length=30, do_sample=False)[0]['summary_text']
+
+    # Özeti hedef dile çevir
+    translated_summary = translator.translate(summary, dest=languages[target_language]).text
+
+    return translated_summary
+
+
+def read_pdf(file):
+    text = ""
+    with open(file.name, 'rb') as f:
+        pdf_reader = PyPDF2.PdfReader(f)
+        num_pages = len(pdf_reader.pages)
+        for page_num in range(num_pages):
+            page = pdf_reader.pages[page_num]
+            text += page.extract_text()
+    return text
+
 
 if st.button("Metni Özetle"):
     input_ids = tokenizer(text_input, return_tensors="pt").input_ids
